0a41317692b3b19f2e22796e8d7b34cdff53f08b
PachecoCesar <c86pacheco@gmail.com>
2019-09-02 12:16:11 -0400

    added new param (title)

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_144131) do
+ActiveRecord::Schema.define(version: 2019_09_02_152027) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "Tittle"
   end
 
 end
